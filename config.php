29fd9ca350564f5662dcdb7ac306164a2273c04b
MaZderMind <github@mazdermind.de>
2015-05-01 12:51:32 +0200

    Zwischenstand by meise

--- config.php
+++ config.php
@@ -22,28 +22,28 @@ $GLOBALS['CONFIG']['CONFERENCE'] = array(
 	 * Dieser im Seiten-Header, im <title>-Tag, in der About-Seite und ggf. ab weiteren Stellen als
 	 * Anzeigetext benutzt
 	 */
-	'TITLE' => '31C3',
+	'TITLE' => '5. Podlove Podcaster Workshop in Berlin am 1.-3. Mai 2015 #ppw15a',
 
 	/**
 	 * Veranstalter
 	 * Wird für den <meta name="author">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
 	 * <meta>-Tag generiert.
 	 */
-	'AUTHOR' => 'CCC',
+	'AUTHOR' => 'Metaebene',
 
 	/**
 	 * Beschreibungstext
 	 * Wird für den <meta name="description">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
 	 * <meta>-Tag generiert.
 	 */
-	'DESCRIPTION' => 'Video Live-Streaming vom 31C3',
+	'DESCRIPTION' => 'Podlove Podcaster Workshop',
 
 	/**
 	 * Schlüsselwortliste, Kommasepariert
 	 * Wird für den <meta name="keywords">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
 	 * <meta>-Tag generiert.
 	 */
-	'KEYWORDS' => '31C3, Hacking, Chaos Computer Club, Video, Media, Streaming, Hacker',
+	'KEYWORDS' => 'Podlove, Podcaster, Workshop Berlin, Video, Media, Streaming',
 
 	/**
 	 * HTML-Code für den Footer (z.B. für spezielle Attribuierung mit <a>-Tags)
@@ -51,9 +51,7 @@ $GLOBALS['CONFIG']['CONFERENCE'] = array(
 	 * Wird diese Zeile auskommentiert, wird die Standard-Attribuierung für (c3voc.de) verwendet
 	 */
 	'FOOTER_HTML' => '
-		by <a href="https://ccc.de">Chaos Computer Club e.V</a>,
-		<a href="http://fem.tu-ilmenau.de/">FeM</a>,
-		<a href="http://www.ags.tu-bs.de/">ags</a> &amp;
+		by <a href="http://metaebene.me">metaebene</a> &amp;
 		<a href="https://c3voc.de">c3voc</a>
 	',
 
@@ -67,13 +65,16 @@ $GLOBALS['CONFIG']['CONFERENCE'] = array(
 	 *
 	 * Wird diese Zeile auskommentiert, wird kein Banner ausgegeben.
 	 */
-	//'BANNER_HTML' => '31C3 – a new dawn',
+	'BANNER_HTML' => '
+		<h2>5. Podlove Podcaster Workshop</h2>
+		<h3>Berlin 1.-3. Mai 2015 #ppw15a</h3>
+	',
 
 	/**
 	 * Link zu den Recordings
 	 * Wird diese Zeile auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELEASES' => 'http://media.ccc.de/browse/congress/2014/index.html',
+	//'RELEASES' => 'https://media.ccc.de/browse/events/datengarten/index.html',
 
 	/**
 	 * Link zu einer (externen) ReLive-Übersichts-Seite
@@ -88,7 +89,7 @@ $GLOBALS['CONFIG']['CONFERENCE'] = array(
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'http://vod.c3voc.de/index.json',
+	//'RELIVE_JSON' => 'http://vod.c3voc.de/index.json',
 
 	/**
 	 * APCU-Cache-Zeit in Sekunden
@@ -110,19 +111,8 @@ $GLOBALS['CONFIG']['OVERVIEW'] = array(
 	 * sonst werden sie nicht angezeigt.
 	 */
 	'GROUPS' => array(
-		'Lecture Rooms' => array(
-			'saal1',
-			'saal2',
-			'saalg',
-			'saal6',
-		),
-
-		'Live DJ Sets'  => array(
-			'lounge',
-			'ambient',
-		),
-		'Live Podcasts' => array(
-			'sendezentrum',
+		'' => array(
+			'MOSAIK',
 		),
 	),
 );
@@ -137,18 +127,18 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 	 * Array-Key ist der Raum-Slug, der z.B. auch zum erstellen der URLs,
 	 * in $GLOBALS['CONFIG']['OVERVIEW'] oder im Feedback verwendet wird.
 	 */
-	'saal1' => array(
+	'MOSAIK' => array(
 		/**
 		 * Angezeige-Name
 		 */
-		'DISPLAY' => 'Saal 1',
+		'DISPLAY' => '5. Podlove Podcaster Workshop',
 
 		/**
 		 * ID des Video/Audio-Streams. Die Stream-ID ist davon abhängig, welches
 		 * Event-Case in welchem Raum aufgebaut wird und wird üblicherweise von
 		 * s1 bis s5 durchnummeriert.
 		 */
-		'STREAM' => 's1',
+		'STREAM' => 's2',
 
 		/**
 		 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen
@@ -164,7 +154,7 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		 * die native-Streams verwendet, andernfalls wird native und translated
 		 * angeboten und auch für beide Tonspuren eine Player-Seite angezeigt.
 		 */
-		'TRANSLATION' => true,
+		'TRANSLATION' => false,
 
 		/**
 		 * stereo-Tonspur statt native-Tonspur benutzen
@@ -198,7 +188,7 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		 * In diesem Fall wird, sofern jeweils aktiviert, Slides, Audio und zuletzt Musik
 		 * als Default-Stream angenommen.
 		 */
-		'HD_VIDEO' => true,
+		'HD_VIDEO' => false,
 
 		/**
 		 * Slide-Only-Stream (1024×576) verfügbar
@@ -209,7 +199,7 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		 * In diesem Fall wird, sofern jeweils aktiviert, Audio und zuletzt Musik als
 		 * Default-Stream angenommen.
 		 */
-		'SLIDES' => true,
+		'SLIDES' => false,
 
 		/**
 		 * Audio-Only-Stream verfügbar
@@ -248,7 +238,7 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		 * Name des Raums im Fahrplan
 		 * Wenn diese Zeile auskommentiert ist wird der Raum-Slug verwendet
 		 */
-		'SCHEDULE_NAME' => 'Saal 1',
+		'SCHEDULE_NAME' => 'MOSAIK',
 
 		/**
 		 * Feedback anzeigen (boolean)
@@ -270,7 +260,7 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		 * Ebenso können alle Subtitles-Funktionialitäten durch auskommentieren
 		 * des globalen $GLOBALS['CONFIG']['SUBTITLES']-Blocks deaktiviert werden
 		 */
-		'SUBTITLES' => true,
+		'SUBTITLES' => false,
 
 		/**
 		 * Embed-Form aktivieren (boolean)
@@ -301,7 +291,7 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		 * Ebenso können alle IRC-Links durch auskommentieren
 		 * des globalen $GLOBALS['CONFIG']['IRC']-Blocks deaktiviert werden
 		 */
-		'IRC' => true,
+		'IRC' => false,
 
 		/**
 		* Mit dem Angaben in diesem Block können die Vorgaben aus dem
@@ -310,10 +300,10 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		* Der globale $GLOBALS['CONFIG']['IRC']-Block muss trotzdem existieren,
 		* da sonst überhaupt kein IRC-Link erzeugt wird. (ggf. einfach `= true` setzen)
 		*/
-		'IRC_CONFIG' => array(
-			'DISPLAY' => '#31C3-hall-1 @ hackint',
-			'URL'     => 'irc://irc.hackint.eu:6667/31C3-hall-1',
-		),
+		//'IRC_CONFIG' => array(
+		//	'DISPLAY' => '#31C3-hall-1 @ hackint',
+		//	'URL'     => 'irc://irc.hackint.eu:6667/31C3-hall-1',
+		//),
 
 		/**
 		 * Twitter-Link aktivieren (boolean)
@@ -339,203 +329,10 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		* da sonst überhaupt kein IRC-Link erzeugt wird. (ggf. einfach `= true` setzen)
 		*/
 		'TWITTER_CONFIG' => array(
-			'DISPLAY' => '#hall1 @ twitter',
-			'TEXT'    => '#31C3 #hall1',
+			'DISPLAY' => '#ppw15a @ twitter',
+			'TEXT'    => '#ppw15a',
 		),
 	),
-
-	'saal2' => array(
-		'DISPLAY' => 'Saal 2',
-		'STREAM' => 's2',
-		'PREVIEW' => true,
-
-		'TRANSLATION' => true,
-		'SD_VIDEO' => true,
-		'HD_VIDEO' => true,
-		'AUDIO' => true,
-		'SLIDES' => true,
-		'MUSIC' => false,
-
-		'SCHEDULE' => true,
-		'SCHEDULE_NAME' => 'Saal 2',
-		'FEEDBACK' => true,
-		'SUBTITLES' => true,
-		'EMBED' => true,
-		'IRC' => true,
-		'IRC_CONFIG' => array(
-			'DISPLAY' => '#31C3-hall-2 @ hackint',
-			'URL'     => 'irc://irc.hackint.eu:6667/31C3-hall-2',
-		),
-		'TWITTER' => true,
-		'TWITTER_CONFIG' => array(
-			'DISPLAY' => '#hall2 @ twitter',
-			'TEXT'    => '#31C3 #hall2',
-		),
-	),
-
-	'saalg' => array(
-		'DISPLAY' => 'Saal G',
-		'STREAM' => 's3',
-		'PREVIEW' => true,
-
-		'TRANSLATION' => true,
-		'SD_VIDEO' => true,
-		'HD_VIDEO' => true,
-		'AUDIO' => true,
-		'SLIDES' => true,
-		'MUSIC' => false,
-
-		'SCHEDULE' => true,
-		'SCHEDULE_NAME' => 'Saal G',
-		'FEEDBACK' => true,
-		'SUBTITLES' => true,
-		'EMBED' => true,
-		'IRC' => true,
-		'IRC_CONFIG' => array(
-			'DISPLAY' => '#31C3-hall-g @ hackint',
-			'URL'     => 'irc://irc.hackint.eu:6667/31C3-hall-g',
-		),
-		'TWITTER' => true,
-		'TWITTER_CONFIG' => array(
-			'DISPLAY' => '#hallg @ twitter',
-			'TEXT'    => '#31C3 #hallg',
-		),
-	),
-
-	'saal6' => array(
-		'DISPLAY' => 'Saal 6',
-		'STREAM' => 's4',
-		'PREVIEW' => true,
-
-		'TRANSLATION' => true,
-		'SD_VIDEO' => true,
-		'HD_VIDEO' => true,
-		'AUDIO' => true,
-		'SLIDES' => true,
-		'MUSIC' => false,
-
-		'SCHEDULE' => true,
-		'SCHEDULE_NAME' => 'Saal 6',
-		'FEEDBACK' => true,
-		'SUBTITLES' => true,
-		'EMBED' => true,
-		'IRC' => true,
-		'IRC_CONFIG' => array(
-			'DISPLAY' => '#31C3-hall-6 @ hackint',
-			'URL'     => 'irc://irc.hackint.eu:6667/31C3-hall-6',
-		),
-		'TWITTER' => true,
-		'TWITTER_CONFIG' => array(
-			'DISPLAY' => '#hall6 @ twitter',
-			'TEXT'    => '#31C3 #hall6',
-		),
-	),
-
-
-	'lounge' => array(
-		'DISPLAY' => 'Lounge',
-		'MUSIC' => true,
-		'EMBED' => true,
-	),
-	'ambient' => array(
-		'DISPLAY' => 'Ambient',
-		'MUSIC' => true,
-		'EMBED' => true,
-	),
-
-
-	'sendezentrum' => array(
-		'DISPLAY' => 'Sendezentrum',
-		'STREAM' => 's5',
-
-		'STEREO' => true,
-		'SD_VIDEO' => true,
-		'HD_VIDEO' => true,
-		'AUDIO' => true,
-
-		'SCHEDULE' => true,
-		'FEEDBACK' => true,
-		'SUBTITLES' => false,
-		'EMBED' => true,
-		'IRC' => false,
-		'TWITTER' => false,
-	),
-);
-
-
-
-/**
- * Konfigurationen zum Konferenz-Fahrplan
- * Wird dieser Block auskommentiert, werden alle Fahrplan-Bezogenen Features deaktiviert
- */
-$GLOBALS['CONFIG']['SCHEDULE'] = array(
-	/**
-	 * URL zum Fahrplan-XML
-	 *
-	 * Diese URL muss immer verfügbar sein, sonst können kann die Programm-Ansicht
-	 * aufhören zu funktionieren. Wenn die Quelle unverlässlich ist ;) sollte ein
-	 * externer HTTP-Cache vorgeschaltet werden.
-	 */
-	'URL' => 'http://events.ccc.de/congress/2014/Fahrplan/schedule.xml',
-
-	/**
-	 * Nur die angegebenen Räume aus dem Fahrplan beachten
-	 *
-	 * Wird diese Zeile auskommentiert, werden alle Räume angezeigt
-	 */
-	//'ROOMFILTER' => array('Saal 1', 'Saal 2', 'Saal G', 'Saal 6'),
-
-	/**
-	 * APCU-Cache-Zeit in Sekunden
-	 * Wird diese Zeile auskommentiert, werden die apc_*-Methoden nicht verwendet und
-	 * der Fahrplan bei jedem Request von der Quelle geladen und geparst
-	 */
-	//'CACHE' => 30*60,
-
-	/**
-	 * Skalierung der Programm-Vorschau in Sekunden pro Pixel
-	 */
-	'SCALE' => 7,
-
-	/**
-	 * Simuliere das Verhalten als wäre die Konferenz bereits heute
-	 *
-	 * Diese folgende Beispiel-Zeile Simuliert, dass das
-	 * Konferenz-Datum 2014-12-29 auf den heutigen Tag 2015-02-24 verschoben ist.
-	 */
-	'SIMULATE_OFFSET' => strtotime(/* Conference-Date */ '2014-12-28') - strtotime(/* Today */ '2015-03-01'),
-	//'SIMULATE_OFFSET' => 0,
-);
-
-
-
-/**
- * Konfiguration des Feedback-Formulars
- *
- * Wird dieser Block auskommentiert, wird das gesamte Feedback-System deaktiviert
- */
-$GLOBALS['CONFIG']['FEEDBACK'] = array(
-	/**
-	 * DSN zum abspeichern der eingegebenen Daten
-	 * die Datenbank muss eine Tabelle enthaltem, die dem in `lib/schema.sql` angegebenen
-	 * Schema entspricht.
-	 *
-	 * Achtung vor Dateirechten: Bei SQLite reicht es nicht, wenn wer Webseiten-Benutzer
-	 * die .sqlite3-Datei schreiben darf, er muss auch im übergeordneten Order neue
-	 * (Lock-)Dateien anlegen dürfen
-	 */
-	'DSN' => 'sqlite:/opt/31c3-streaming-feedback/feedback.sqlite3',
-
-	/**
-	 * Login-Daten für die /feedback/read/-Seite, auf der eingegangenes
-	 * Feedback gelesen werden kann.
-	 *
-	 * Durch auskommentieren der beiden Optionen wird diese Seite komplett deaktiviert,
-	 * es kann dann nur noch durch manuelle Inspektion der .sqlite3-Datei auf das Feedback
-	 * zugegriffen werden.
-	 */
-	'USERNAME' => 'winke',
-	'PASSWORD' => 'katze',
 );
 
 /**
@@ -545,19 +342,6 @@ $GLOBALS['CONFIG']['FEEDBACK'] = array(
  * Embedding-Funktionen deaktiviert.
  */
 $GLOBALS['CONFIG']['EMBED'] = true;
-
-/**
- * Konfiguration des L2S2-Systems
- * https://github.com/c3subtitles/L2S2
- *
- * Wird dieser Block auskommentiert, wird das gesamte Subtitle-System deaktiviert
- */
-$GLOBALS['CONFIG']['SUBTITLES'] = array(
-	/**
-	 * URL des L2S2-Servers
-	 */
-	'URL' => 'http://subtitles.c3voc.de/',
-);
 
 /**
  * Globale Konfiguration der IRC-Links.
@@ -576,7 +360,7 @@ $GLOBALS['CONFIG']['IRC'] = array(
 	 * Ist eine weitere Anpassung erfoderlich, kann ein IRC_CONFIG-Block in der
 	 * Raum-Konfiguration zum Überschreiben dieser Angaben verwendet werden.
 	 */
-	'DISPLAY' => '#31C3-%s @ hackint',
+	'DISPLAY' => '#ccc @ ircnet',
 
 	/**
 	 * URL für die IRC-Links.
@@ -587,7 +371,7 @@ $GLOBALS['CONFIG']['IRC'] = array(
 	 * %s wird durch den urlencodeten Raum-Slug ersetzt.
 	 * Eine Anpassung kann ebenfalls in der Raum-Konfiguration vorgenommen werden.
 	 */
-	'URL' => 'irc://irc.hackint.eu:6667/31C3-%s',
+	'URL' => 'irc://irc.de.ircnet.net/ccc',
 );
 
 /**
@@ -607,7 +391,7 @@ $GLOBALS['CONFIG']['TWITTER'] = array(
 	 * Ist eine weitere Anpassung erfoderlich, kann ein TWITTER_CONFIG-Block in der
 	 * Raum-Konfiguration zum Überschreiben dieser Angaben verwendet werden.
 	 */
-	'DISPLAY' => '#%s @ twitter',
+	'DISPLAY' => '#ppw15a @ twitter',
 
 	/**
 	 * Vorgabe-Tweet-Text für die Twitter-Links.
@@ -615,5 +399,38 @@ $GLOBALS['CONFIG']['TWITTER'] = array(
 	 * %s wird durch den Raum-Slug ersetzt.
 	 * Eine Anpassung kann ebenfalls in der Raum-Konfiguration vorgenommen werden.
 	 */
-	'TEXT' => '#31C3 #%s',
+	'TEXT' => '#ppw15a',
+);
+
+/**
+ * Konfigurationen zum Konferenz-Fahrplan
+ * Wird dieser Block auskommentiert, werden alle Fahrplan-Bezogenen Features deaktiviert
+ */
+$GLOBALS['CONFIG']['SCHEDULE'] = array(
+	/**
+	 * URL zum Fahrplan-XML
+	 *
+	 * Diese URL muss immer verfügbar sein, sonst können kann die Programm-Ansicht
+	 * aufhören zu funktionieren. Wenn die Quelle unverlässlich ist ;) sollte ein
+	 * externer HTTP-Cache vorgeschaltet werden.
+	 */
+	'URL' => 'http://www.fossgis.de/konferenz/2015/programm/schedule.de.xml',
+	/**
+	 * APCU-Cache-Zeit in Sekunden
+	 * Wird diese Zeile auskommentiert, werden die apc_*-Methoden nicht verwendet und
+	 * der Fahrplan bei jedem Request von der Quelle geladen und geparst
+	 */
+	//'CACHE' => 30*60,
+	/**
+	 * Skalierung der Programm-Vorschau in Sekunden pro Pixel
+	 */
+	'SCALE' => 7,
+	/**
+	 * Simuliere das Verhalten als wäre die Konferenz bereits heute
+	 *
+	 * Diese folgende Beispiel-Zeile Simuliert, dass das
+	 * Konferenz-Datum 2014-12-29 auf den heutigen Tag 2015-02-24 verschoben ist.
+	 */
+	//'SIMULATE_OFFSET' => strtotime(/* Conference-Date */ '2015-03-11') - strtotime(/* Today */ '2015-03-03'),
+	'SIMULATE_OFFSET' => 0,
 );
